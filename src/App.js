fa62a01407ffeb2dc8714321c2440c622c352b83
jeqiu <74211080+jeqiu@users.noreply.github.com>
2022-02-26 20:10:11 -0800

    create separate component for quick picks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Route, Switch, Link, useLocation } from 'react-router-dom';
 import { Container, Row, Col, Form, InputGroup, Button, Navbar, Nav } from 'react-bootstrap';
 import QuickPickForm from './components/QuickPickForm';
-import RandomRecipe from './components/RandomRecipe';
 import AllRecipes from './components/AllRecipes';
 import SavedRecipe from './components/SavedRecipe';
 import LoginForm from './components/LoginForm';
@@ -10,17 +9,12 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import recipeService from './requests/recipe';
 import userService from './requests/user';
-import { TailSpin } from  'react-loader-spinner';
 import '../node_modules/react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 
 
 const App = () => {
   const location = useLocation();
-  const [recipe, setRecipe] = useState({});
-  const [searchInput, setsearchInput] = useState('');
-  const [isLoading, setLoading] = useState(false);
-  const [imgLoading, setImgLoading] = useState(false);
   
   const [usernameInput, setUsername] = useState('');
   const [user, setUser] = useState('');
@@ -29,32 +23,6 @@ const App = () => {
   useEffect(() => {
     console.log(`You changed the page to: ${location.pathname}`);
   }, [location]);
-
-
-  const getRecipe = (event) => {
-    event.preventDefault();
-    setLoading(true);
-    setImgLoading(true);
-
-    if (searchInput) {
-      // input contains intolerances
-      recipeService
-      .getSearchRecipe(searchInput)
-      .then(newRecipe=>setRecipe(newRecipe))
-      .then(() => setLoading(false));
-      return;
-    } else {
-      recipeService
-      .getRandomRecipe()
-      .then(newRecipe=>setRecipe(newRecipe))
-      .then(() => setLoading(false));
-    }
-  };
-
-  const handleSearchInputChange = (event) => {
-    console.log(event.target.value);
-    setsearchInput(event.target.value);
-  };
 
   const handleUsernameInput = (event) => {
     console.log(event.target.value);
@@ -110,59 +78,8 @@ const App = () => {
 
       <Header title='Quick-Pick Recipes' subtitle='A random suggestion for your next recipe!' />
 
-      {/* <QuickPickForm 
-        getRecipe={getRecipe}
-        searchInput={searchInput}
-        handleSearchInputChange={handleSearchInputChange}
-        isLoading={isLoading}
-      /> */}
-      
-      <Row 
-      className="justify-content-center text-center"
-      style={{margin: '1rem'}}
-      >
-      <Col md={{ span: 6 }}>
-      <Form onSubmit={getRecipe} >
-      <InputGroup>
-        <InputGroup.Text htmlFor='recipe-search-input'>Exclude:</InputGroup.Text>
-        <Form.Control 
-          type='text'
-          id='recipe-search-input'
-          placeholder='any intolerances e.g. peanuts'
-          pattern='^$|(([a-z]|[A-Z])*,?\s?)*'
-          value={searchInput}
-          onChange={handleSearchInputChange}
-        />
-        <Button 
-          variant='primary' 
-          type='submit'
-          disabled={isLoading}
-        >
-          {isLoading ? 'Get A New Recipe' : 'Get A New Recipe'}
-        </Button>
-        </InputGroup>
-      </Form> 
-      </Col>
-      </Row>
-      
-        {!isLoading && (
-          <Row className="justify-content-center">
-            <RandomRecipe
-            title={recipe.title} 
-            image={recipe.image} 
-            sourceUrl={recipe.sourceUrl}
-            ingredients={recipe.extendedIngredients}
-            imgLoading={imgLoading}
-            setImgLoading={setImgLoading} 
-            />
-          </Row>
-        )}
-
-        {imgLoading && (
-          <Row className="justify-content-center text-center">
-            <TailSpin color="#00BFFF" height={100} width={100} />
-          </Row>
-        )}
+      <QuickPickForm />
+  
       </div>
       </Route>
       </Switch>
--- src/components/QuickPickForm.js
+++ src/components/QuickPickForm.js
@@ -1,38 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Form, InputGroup, Button } from 'react-bootstrap';
+import RandomRecipe from './RandomRecipe';
+import recipeService from '../requests/recipe';
+import { TailSpin } from  'react-loader-spinner';
 
-const QuickPickForm = ({ getRecipe, searchInput, handleSearchInputChange, isLoading }) => {
-  
+const QuickPickForm = () => {
+  const [recipe, setRecipe] = useState({});
+  const [imgLoading, setImgLoading] = useState(false);
+  const [searchInput, setsearchInput] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  const getRecipe = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setImgLoading(true);
+
+    if (searchInput) {
+      // input contains intolerances
+      recipeService
+      .getSearchRecipe(searchInput)
+      .then(newRecipe=>setRecipe(newRecipe))
+      .then(() => setLoading(false));
+      return;
+    } else {
+      recipeService
+      .getRandomRecipe()
+      .then(newRecipe=>setRecipe(newRecipe))
+      .then(() => setLoading(false));
+    }
+  };
+
+  const handleSearchInputChange = (event) => {
+    console.log(event.target.value);
+    setsearchInput(event.target.value);
+  };
+
+
   return (
+    <>
     <Row 
-    className="justify-content-center text-center"
-    style={{margin: '1rem'}}
+      className="justify-content-center text-center"
+      style={{margin: '1rem'}}
     >
-    <Col md={{ span: 6 }}>
-    <Form onSubmit={getRecipe} >
-    <InputGroup>
-      {/* <Form.Label htmlFor='recipe-search-input'>Exclude: </Form.Label> */}
-      <InputGroup.Text htmlFor='recipe-search-input'>Exclude:</InputGroup.Text>
-      <Form.Control 
-        type='text'
-        id='recipe-search-input'
-        placeholder='any intolerances e.g. peanuts'
-        pattern='^$|(([a-z]|[A-Z])*,?\s?)*'
-        value={searchInput}
-        onChange={handleSearchInputChange}
-      />
-      <Button 
-        // className="w-50"
-        variant='primary' 
-        type='submit'
-        disabled={isLoading}
-      >
-        {isLoading ? 'Get A New Recipe' : 'Get A New Recipe'}
-      </Button>
+      <Col md={{ span: 6 }}>
+      <Form onSubmit={getRecipe} >
+      <InputGroup>
+        {/* <Form.Label htmlFor='recipe-search-input'>Exclude: </Form.Label> */}
+        <InputGroup.Text htmlFor='recipe-search-input'>Exclude:</InputGroup.Text>
+        <Form.Control 
+          type='text'
+          id='recipe-search-input'
+          placeholder='any intolerances e.g. peanuts'
+          pattern='^$|(([a-z]|[A-Z])*,?\s?)*'
+          value={searchInput}
+          onChange={handleSearchInputChange}
+        />
+        <Button 
+          // className="w-50"
+          variant='primary' 
+          type='submit'
+          disabled={isLoading}
+        >
+          {isLoading ? 'Get A New Recipe' : 'Get A New Recipe'}
+        </Button>
       </InputGroup>
-    </Form> 
-    </Col>
+      </Form> 
+      </Col>
     </Row>
+      
+    {!isLoading && (
+      <Row className="justify-content-center">
+        <RandomRecipe
+          title={recipe.title} 
+          image={recipe.image} 
+          sourceUrl={recipe.sourceUrl}
+          ingredients={recipe.extendedIngredients}
+          imgLoading={imgLoading}
+          setImgLoading={setImgLoading} 
+        />
+      </Row>
+      )}
+
+    {imgLoading && (
+      <Row className="justify-content-center text-center">
+        <TailSpin color="#00BFFF" height={100} width={100} />
+      </Row>
+    )}
+    </>
   )
 }
 
