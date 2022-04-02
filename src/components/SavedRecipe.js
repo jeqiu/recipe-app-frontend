13de1b9130da82b3154f03a46a87679d1ec1dc74
jeqiu <74211080+jeqiu@users.noreply.github.com>
2022-04-02 00:02:43 -0700

    fixed render issue

--- src/components/SavedRecipe.js
+++ src/components/SavedRecipe.js
@@ -1,51 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Row, Col, Image, Button } from 'react-bootstrap';
 import recipeService from '../services/recipe';
 
 const SavedRecipe = ({ recipe_id, title, image, sourceUrl }) => {
+  const [recipeDisplay, setRecipeDisplay] = useState(true);
+
   const handleDelete = async () => {
     try {
       const res = await recipeService.deleteRecipe(recipe_id);
       console.log(res);
+      setRecipeDisplay(false);
     } catch (error) {
       console.log(error);
     }
   }
 
-  return (
-    <Row 
-    style={{paddingTop: '1rem'}}
-    className="align-items-center justify-content-center"
-    >
-      <Col
-        sm={{ span: 4 }}
+  if (recipeDisplay) {
+    return (
+      <Row 
+      style={{paddingTop: '1rem'}}
+      className="align-items-center justify-content-center"
       >
-        <Image src={image} alt={title} rounded="true" fluid="true" />
-      </Col>
-      <Col sm={{ span: 8 }}>
-        <Row>
-          <Col style={{fontSize: '1.25rem'}}>
-            <b>{title}</b>
+        <Col
+          sm={{ span: 4 }}
+        >
+          <Image src={image} alt={title} rounded="true" fluid="true" />
+        </Col>
+        <Col sm={{ span: 8 }}>
+          <Row>
+            <Col style={{fontSize: '1.25rem'}}>
+              <b>{title}</b>
+            </Col>
+          </Row>
+          <Row style={{paddingTop: '0.5rem'}}>
+            <Col><a href={sourceUrl} target="_blank" rel="noopener noreferrer">
+              <Button>View Full Recipe</Button>
+            </a></Col>
+          </Row>
+          <Row style={{paddingTop: '0.5rem'}}>
+          <Col>
+            <Button 
+              variant="secondary"
+              onClick={handleDelete}
+            >
+              Delete
+            </Button>
           </Col>
         </Row>
-        <Row style={{paddingTop: '0.5rem'}}>
-          <Col><a href={sourceUrl} target="_blank" rel="noopener noreferrer">
-            <Button>View Full Recipe</Button>
-          </a></Col>
-        </Row>
-        <Row style={{paddingTop: '0.5rem'}}>
-        <Col>
-          <Button 
-            variant="secondary"
-            onClick={handleDelete}
-          >
-            Delete
-          </Button>
         </Col>
       </Row>
-      </Col>
-    </Row>
-  )
+    )
+  } else {
+    return null;
+  }
 }
 
 export default SavedRecipe
